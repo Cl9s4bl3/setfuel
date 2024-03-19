3eed1bc82a12c693fa5a38f74d7dbb1b6d195432
Cl9s4bl3 <126987836+Cl9s4bl3@users.noreply.github.com>
2024-03-19 19:32:48 +0100

    Add files via upload

--- client.lua
+++ client.lua
@@ -2,6 +2,15 @@ RegisterNetEvent('setFuel')
 AddEventHandler('setFuel', function(fuelValue)
     local vehicle = GetVehiclePedIsIn(PlayerPedId(), false)
     if vehicle ~= 0 then
+
+        if fuelValue >100 then
+            --TriggerEvent('chat:addMessage', { args = { '^5SYSTEM', 'Invalid number! (0-100)'} })
+            return exports["k5_notify"]:notify('Fuel', 'Invalid number! (0-100)', 'error')
+        elseif fuelValue <1 then
+            --TriggerEvent('chat:addMessage', { args = { '^5SYSTEM', 'Invalid number! (0-100)'} })
+            return exports["k5_notify"]:notify('Fuel', 'Invalid number! (0-100)', 'error')
+        end
+
         SetVehicleFuelLevel(vehicle, fuelValue + 0.0)
         --TriggerEvent('chat:addMessage', { args = { '^5SYSTEM', ' Vehicle fuel set to ' .. fuelValue } })
         exports["k5_notify"]:notify('Fuel', 'Successfully set vehicle fuel level to '..fuelValue, 'success')
